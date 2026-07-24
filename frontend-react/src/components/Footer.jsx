47570beda85c4ef1db7561f29f129942677e9484
Sanjana Ramgarhia <sanjanaramgarhia@gmail.com>
2026-07-24 18:22:34 +0530

    Homepage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className='footer py-3 my-4'>
+                <hr className='border-bottom' />
+                <p className='text-light text-center'>&copy; 2026 - Built with ❤️ by Sanjana Ramgarhia </p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
