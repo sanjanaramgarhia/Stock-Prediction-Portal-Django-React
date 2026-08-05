1672d3ebd318826d7cc31400232a5da438c25eed
Sanjana Ramgarhia <sanjanaramgarhia@gmail.com>
2026-08-05 18:51:14 +0530

    Login Functionality using JWT

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,29 +1,59 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+    const [error, setError] = useState('')
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const userData = {
+            username, password
+        }
+        console.log('userData==>', userData)
+
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/v1/token/', userData)
+            localStorage.setItem('accessToken', response.data.access)
+            localStorage.setItem('refreshToken', response.data.refresh)
+            console.log('Login successful');
+            navigate('/')
+        }
+        catch (error) {
+            console.error('Invalid Credentials')
+            setError('Invalid credentials')
+        } finally {
+            setLoading(false)
+        }
+
+    }
 
     return (
         <>
             <div className='container'>
                 <div className="row justify-content-center">
                     <div className="col-md-6 bg-light-dark p-5 rounded">
-                        <h3 className='text-light text-center mb-4'>Create an Account</h3>
-                        <form>
+                        <h3 className='text-light text-center mb-4'>Login to our Portal</h3>
+                        <form onSubmit={handleLogin}>
                             <div className='mb-3'>
                                 <input type="text" className='form-control' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
-                                <small>{errors.username && <div className='text-danger'>{errors.username}</div>}</small>
                             </div>
                             <div className='mb-3'>
                                 <input type="password" className='form-control ' placeholder='Set password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                                <small>{errors.password && <div className='text-danger'>{errors.password}</div>}</small>
                             </div>
+                            {error && <div className='text-danger'>{error}</div>}
                             {loading ? (
-                                <button type='submit' className='btn btn-info d-block mx-auto' disabled><FontAwesomeIcon icon={faSpinner} spin /> Please wait...</button>
+                                <button type='submit' className='btn btn-info d-block mx-auto' disabled><FontAwesomeIcon icon={faSpinner} spin /> Logging In...</button>
                             ) : (
-                                <button type='submit' className='btn btn-info d-block mx-auto'>Register</button>
+                                <button type='submit' className='btn btn-info d-block mx-auto'>Login</button>
                             )}
 
                         </form>
