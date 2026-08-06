eccbf5af0020c39f4d61bb8dd08d86be9b78ead2
Sanjana Ramgarhia <sanjanaramgarhia@gmail.com>
2026-08-06 18:57:19 +0530

    Login Functionality

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
 
 const Login = () => {
     const [username, setUsername] = useState('')
@@ -10,6 +11,7 @@ const Login = () => {
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
     const [error, setError] = useState('')
+    const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -25,6 +27,7 @@ const Login = () => {
             localStorage.setItem('accessToken', response.data.access)
             localStorage.setItem('refreshToken', response.data.refresh)
             console.log('Login successful');
+            setIsLoggedIn(true)
             navigate('/')
         }
         catch (error) {
